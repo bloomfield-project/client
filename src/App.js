b59dd1186b3d2edc6f6ca47c08a48e77a73ca71e
unknown <gihanweerasinghe21@gmail.com>
2022-07-24 21:21:47 +0530

    header component ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
 
 function App() {
   return (
-    <div className="App">
-    
+  <div className="App">
+    <Header>
+    </Header>
   </div>
 );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import logo from "./bf.png-removebg-preview.png";
+import profpic from "./profpic.jfif"
+import { BsBell } from "react-icons/bs";
+
+function Header(){
+    return (
+      <div className="container-1">
+        <section className='header-left'>
+          <div className="image">
+            <img src={logo}></img>
+          </div>
+          <div className="bloomfield">
+            <text>BLOOMFIELD</text>
+          </div>
+        </section>
+        <section className='header-right'>
+          <div className='notify'>
+            <BsBell className='bell'></BsBell>
+          </div>
+          <div className='username'>
+            <text>W.D.K.G.G.S.Perera</text>
+          </div>
+          <div className='profilePic'>
+            <img src={profpic}></img>
+          </div>
+        </section>
+      </div>
+  );
+}
+
+export default Header;
