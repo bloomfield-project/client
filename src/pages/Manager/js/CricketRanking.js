6827b7665410a506054d6fb4927c2468c8830b7c
unknown <gihanweerasinghe21@gmail.com>
2022-07-31 03:04:49 +0530

    ranking in progress

--- src/pages/Manager/js/CricketRanking.js
+++ src/pages/Manager/js/CricketRanking.js
@@ -18,22 +18,28 @@ function CricketRanking() {
                 <h1>BLOOMFIELD Cricket Rankings</h1>
               </div>
               <div className="tabs">
-                <h5 className="tab">Batting<hr></hr></h5>
+                <h5 className="tab-active">Batting<hr></hr></h5>
                 <h5 className="tab">Balling</h5>
                 <h5 className="tab">All-rounders</h5>
-                
               </div>
               <hr></hr>
               <div className="table-box-1">
                 <div className="table-tabs">
-
+                    <div className="table-tab">ODI</div>
+                    <div className="table-tab-active">T20</div>
+                    <div className="table-tab">TEST</div>
                 </div>
-                <div className="table">
+                <div className="tablee">
                     <div className="table-head">
-
+                        <div className="col-1">Position</div>
+                        <div className="col-1">Player</div>
+                        <div className="col-1">Rating</div>
                     </div>
                     <div className="table-row">
-
+                        <div className="col-1">1</div>
+                        <div className="col-1">Sameera Madushan</div>
+                        <div className="col-1"><button>view</button></div>
+                        <hr></hr>
                     </div>
                 </div>
 
