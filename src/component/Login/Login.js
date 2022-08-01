a169088e8b45c754f240a40c5a6e167fb922048d
unknown <gihanweerasinghe21@gmail.com>
2022-08-01 17:10:36 +0530

    matyhk na

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -9,7 +9,7 @@ function Login() {
       <div className="login-left">
         <h1 className="title">BLOOMFIELD</h1>
         <p className="description">Bloomfield Cricket & Athletic club: <br></br>Member management System</p>
-        <img src={cricket} alt="cricket" ></img>
+        <img className="login-img" src={cricket} alt="cricket" ></img>
       </div>
       {/* <div className="login-right">
         <LoginForm/>
