7cb8cea448f6e3002d58c46d8b4005dc112958bc
VNucsc0 <nayanahari1234@gmail.com>
2022-08-19 21:07:48 +0530

    AdminAccounts

--- src/pages/naya_admin/js/A_Accounts.js
+++ src/pages/naya_admin/js/A_Accounts.js
@@ -100,9 +100,63 @@ const data_1 = [
     btn: <Button variant="secondary">View</Button>,
    },
 ];
+function A_Accounts() {
+    const[tabNumber, setTabNumber]  = useState(1);
+    const selectTab_1 = ()=>{
+        setTabNumber(1);
+    }
+    const selectTab_2 = ()=>{
+        setTabNumber(2);
+    }
+    return (
+        <>
+        <div className="page-container-1">
+            <div className="header-container">
+                <Header></Header>
+            </div>
+            <div className="body-container-1">
+                <div className="navbar-container">
+                    <Navbar></Navbar>
+                </div>
+                <div className="body-container-2">
+                    <div className="title">
+                        <h1>{tabNumber === 1 ?"Employees" : "Players"}</h1>
+                    </div>
+                    <div className="tabs">
+                        <div className="tabs-left">
+                            <h5 className= {tabNumber === 1 ? "tab-active" : "tab"}><a onClick={()=>selectTab_1(1)}>Employees</a> {tabNumber === 1 ? <hr></hr> : ""}</h5>
+                            <h5 className= {tabNumber === 2 ? "tab-active" : "tab"}><a onClick={()=>selectTab_2(2)}>Players</a> {tabNumber === 2 ? <hr></hr> : ""}</h5>
+                            </div>
 
-    
+                            <div className="tabs-right">
+                                <Button variant="outline-success">+ Add</Button>
+                            </div>
+                            </div>
+                            
+                            <hr></hr>
+                            <div className="table-box-1">
+                                <div className="tablee">
+                                    <SearchTable
+                                    title={false}
+                                    data={tabNumber === 2 ? data : data_1}
+                                    columns={tabNumber === 2 ? columns : columns_1}
+                                    searching={true}
+                                    sort={false}
+                                    filter={false}
+                                    paging={true}
+                                    headerC={"#4a4a4a"}
+                                    headerH={"40px"}
+                                    headerFC={'white'}
+                                    headerFS={'1.2rem'}
+                                    headerFW={'500'}
+                                    />
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
 
-   
-
-    
+export default A_Accounts;
